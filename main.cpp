f3129a71b7de0f67382a170619bda46a40f524ed
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-10-17 19:52:03 +0300

    1

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    double L;
+
+    std::cout << "Enter circumference L: ";
+    std::cin >> L;
+
+    double R = L / (2 * 3.14);
+
+    double S = 3.14 * R * R;
+
+    std::cout << "Circle radius R: " << R << std::endl;
+    std::cout << "Area of a circle S: " << S << std::endl;
+
+    return 0;
+}
